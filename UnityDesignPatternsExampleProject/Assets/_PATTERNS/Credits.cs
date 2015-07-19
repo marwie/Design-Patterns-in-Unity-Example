86fdf1bfc0e0654e87c92054e9f1e3613c090813
Marcel Wiessler <marwie@users.noreply.github.com>
2015-07-19 13:57:04 +0200

    Update Credits.cs

--- UnityDesignPatternsExampleProject/Assets/_PATTERNS/Credits.cs
+++ UnityDesignPatternsExampleProject/Assets/_PATTERNS/Credits.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * 
- * ALL CREDIT belongs to Derek Barnas
+ * ALL CREDIT belongs to Derek Banas
  * 
  * I just followed his video tutorial series and tried to translate everything to C# in Unity
  * You can find his great series on Design Patterns here: https://www.youtube.com/playlist?list=PLF206E906175C7E07
